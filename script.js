8fea3920b5242edefb5570ee17a71ceea0b0c175
Diptanu Baidya <129202457+Diptanu12@users.noreply.github.com>
2024-11-25 22:46:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function firstWord(s) {
-  // your code here
+    s = s.trim();
+    const spaceIndex = s.indexOf(' ');
+    return spaceIndex === -1 ? s : s.substring(0, spaceIndex);
 }
-
-// Do not change the code below
-
+ 
 const s = prompt("Enter String:");
 alert(firstWord(s));
